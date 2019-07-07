2ef322e03bd0855fa08a57f5ba144a8632aabd7d
xiaoran han <xiaoranhann@gmail.com>
2019-07-08 02:42:59 +0900

    [home] backend, add search movie, search movie by title function.

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,32 @@
 const express = require("express");
 const router = express.Router();
+const request = require("request");
+const config = require("../config/keys");
+
+const APIKEY = config.APIKEY;
 
 router.get("/", (req, res) => res.send("hello, world"));
 
-router.get("/dashboard", (req, res) => res.send("dashboard"));
+// search movie
+router.get("/search", function(req, res) {
+  var query = req.query.search;
+  var url = "https://www.omdbapi.com/?s=" + query + "&apikey=" + APIKEY;
+  request(url, function(request, response) {
+    if (response.statusCode == 200) {
+      res.send(JSON.parse(response.body));
+    }
+  });
+});
+
+// search movie by title
+router.get("/results", function(req, res) {
+  var query = req.query.title;
+  var url = "https://www.omdbapi.com/?t=" + query + "&apikey=" + APIKEY;
+  request(url, function(request, response) {
+    if (response.statusCode == 200) {
+      res.send(JSON.parse(response.body));
+    }
+  });
+});
 
 module.exports = router;
